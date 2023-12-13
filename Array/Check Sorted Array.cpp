81fd606b19de05db4e69d8e364941049b9bab40c
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-14 01:46:31 +0530

    Update Check Sorted Array.cpp

--- Array/Check Sorted Array.cpp
+++ Array/Check Sorted Array.cpp
@@ -7,3 +7,15 @@ for(int i=0;i<n-1;i++){
   }
    return 1;
 }
+
+
+second method its mine method
+int isSorted(int n, vector<int> arr) {
+    // Write your code here.
+
+for(int i=0;i<n;i++){
+    if(arr[i-1]>arr[i])
+    return 0;
+  }
+   return 1;
+}
